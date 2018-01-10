43cd2c4198f9a084622ed9d4b31346aeb2009421
amstersean <samster@frontlinetechnologies.com>
2018-01-10 12:18:30 -0500

    adding condition if it link doesn't exist

--- overwriteLinkUTM.js
+++ overwriteLinkUTM.js
@@ -1,16 +1,17 @@
 function overwriteLinkUTM() {
     var link = $(".utmOverwrite");
-    var l = link.attr('href');
-    var a = l.split("?"); 
-
-    var params = {
-        utm_source : Cookies.get('utm_source'), 
-        utm_medium : Cookies.get('utm_medium'),
-        utm_campaign : Cookies.get('utm_campaign'),
-        utm_content : Cookie.get('utm_content')
+    if(link){
+        var l = String(link.attr('href'));
+        var a = l.split("?"); 
+    
+        var params = {
+            utm_source : Cookies.get('utm_source'), 
+            utm_medium : Cookies.get('utm_medium'),
+            utm_campaign : Cookies.get('utm_campaign'),
+            utm_content : Cookie.get('utm_content')
+        }
+        link.attr('href', a[0] + "?" + encodeQueryData(params));
     }
-    link.attr('href', a[0] + "?" + encodeQueryData(params));
-
 }
 
 function encodeQueryData(data) {
@@ -19,5 +20,6 @@ function encodeQueryData(data) {
       ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
     return ret.join('&');
  }
+
  
  //Justs needs to call overwriteLinkUTM() after the cookies are init
